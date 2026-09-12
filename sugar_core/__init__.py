b8344d2c96e9404cdf72b5fee5fc8894485b28f9
Alejandro Grenier <53024346+Jandro22@users.noreply.github.com>
2026-09-12 14:47:02 -0400

    Expose high-volume harvest API

--- sugar_core/__init__.py
+++ sugar_core/__init__.py
@@ -11,6 +11,14 @@ from .collector_registry import (
     fetch_registered_item,
     get_collector,
 )
+from .harvest import (
+    HarvestConfig,
+    HarvestStore,
+    HarvestTask,
+    build_harvest_tasks,
+    rate_limit_wait_seconds,
+    run_harvest,
+)
 from .models import COLLECTOR_VERSION, SCHEMA_VERSION, PostRecord
 from .observation_storage import load_observations, observations_to_frame, save_observations
 from .observations import (
@@ -35,6 +43,12 @@ __all__ = [
     "collect_registered_source",
     "fetch_registered_item",
     "collect_registered_comments",
+    "HarvestConfig",
+    "HarvestStore",
+    "HarvestTask",
+    "build_harvest_tasks",
+    "rate_limit_wait_seconds",
+    "run_harvest",
     "ResearchObservation",
     "EvidenceReference",
     "OBSERVATION_SCHEMA_VERSION",
